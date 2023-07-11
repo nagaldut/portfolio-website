466a9aa9f01a3a7d719d7af125dd39aa7c344a45
NehaRani Agalduty <125945418+nagaldut@users.noreply.github.com>
2023-07-10 22:33:57 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+const main = {
+  name: 'Neha Rani Agalduty',
+  mail: 'Neharani1727@gmail.com',
+  img: 'user.png',
+  role: ['Data Scientist'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/sivanesh-s'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/sivanesh-shanmugam/'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'neharani1727@@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: 'https://github.com/Devsendatha' },
+    { name: 'Blogs', link: 'https://medium.com/@devsendatham' },
+    { name: 'Resume', link: 'https://www.dropbox.com/s/63cubltrlgrr76b/Devsen%20Datha%20Mukkamala%20Resume.pdf?dl=0'},
+  ]
+};
